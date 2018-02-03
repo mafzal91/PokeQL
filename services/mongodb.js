c2dddc6a6694a789bbee80b8fb62b4fc160f0328
Mohammad Afzal <mafzal91@gmail.com>
2018-02-02 20:27:20 -0500

    update

--- services/mongodb.js
+++ services/mongodb.js
@@ -1,7 +1,7 @@
 var config = require('../config')
 var mongo = require('mongoose')
 mongo.Promise = global.Promise;
-mongo.connect(`mongodb://${config.mongodb.hosts[0]}/${config.mongodb.database}`, {useMongoClient: true}).then(
+mongo.connect(`mongodb://${config.mongodb.hosts[0]}:${config.mongodb.port}/${config.mongodb.database}`, {useMongoClient: true}).then(
   () => { console.log("Connected")},
   (err) => { console.log(err)}
 )
